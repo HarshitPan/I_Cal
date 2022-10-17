63be96761bb0514e3216c5d8d2f18b64afdca63f
harsh <harshitpandeyfeelings@gmail.com>
2022-10-17 12:32:32 +0530

    This is a commit

---
+++ eclipse-workspace/MyCal/src/MyCalPkg/Main.java
@@ -0,0 +1,11 @@
+package MyCalPkg;
+
+public class Main {
+	public static void main(String args[])
+	{
+		Calculator obj=new Calculator();
+		int a=10,b=20;
+		System.out.println(obj.add(a,b));
+	}
+
+}
